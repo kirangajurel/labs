2ca0f156747b00ec22c42759425b162b2c0f98ea
nepalsanto <nep.santo001@gmail.com>
2018-03-18 18:34:37 +0545

    Add files via upload

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,15 +1,12 @@
-#include<iostream>
-#include<cmath>
-using namespace std;
-enum days{Sun,Mon,Tue,Wed,Thu,Fri,Sat};
-int main(){
-	days d1,d2;
-	d1= Fri;
-	d2= Mon;
-	if (d1<d2)
-		cout<< (d1) <<"comes before"<< (d2)<<endl;
-	else 
-		cout<< (d2) <<"comes before "<< (d1)<< endl ;
-	cout<<" the number of working days is "<<abs((d2-d1)/3);
-return 0;
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,n;
+    printf("Enter a natural number:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    printf("The natural number from 1 to n is:%d",i);
+    return 0;
+	
 }
--- num_func_overload.cpp
+++ num_func_overload.cpp
@@ -1,24 +1,23 @@
 #include<iostream>
 using namespace std;
-int show(int);
+void show(int);
 void show(int,int);
 void show(int,int,int);
 int main(){
 	int a,b,c;
 	a=10;b=20;c=30;
-	cout<<"single function is envoked"<<show(a)<<endl;
+	cout<<"single function is envoked"<<endl;show(a);
 	cout<<endl<<"double function is envoked"<<endl;show(b,c);
 	cout<<endl<<"triple function is envoked"<<endl;show(a,b,c);
 	//cout<<"character function is envoked"<<endl<<show;
 return 0;
 }
-int show(int a){
+void show(int a){
 	cout<<"1	"<<a;
-	return a;
 }
 void show(int num, int nu){
 	cout<<"2 	"<<num+nu;
 }
 void show(int a,int b,int c){
 	cout<<"3	"<<a+b+c;
-}
+.}
